d06b572664fdc332881777d5226fccb70a72b724
tsalair <toplider888@mail.ru>
2023-10-03 20:09:46 +0300

    Task_1_changed2

--- Task_1.py
+++ Task_1.py
@@ -2,25 +2,17 @@ import requests
 
 def most_intelligence(hero_list):
     all_heroes_list = requests.get('https://akabab.github.io/superhero-api/api/all.json').json()
-    all_heroes_list_tuples = []
-    for item in all_heroes_list:
-        hero_tuple = list(item.items())
-        all_heroes_list_tuples.append(hero_tuple)
-    
+ 
     intel_list = []
-    for hero in hero_list:
-        for item in all_heroes_list_tuples:
-            name = item[1][1]
-            intel = list(item[3])[1]['intelligence']
-            
-            if name == hero:
-                intel_list.append([name, intel])
+    for item in all_heroes_list:
+        name = item['name'] 
+        if name in hero_list:
+       
+            intel = item['powerstats']['intelligence']
+            intel_list.append([intel, name])
 
-    def int_key(l):
-        return l[1]
+    intel_list.sort(reverse=True)
 
-    intel_list.sort(key=int_key, reverse=True)
-
-    print(f'Самый умный герой {intel_list[0][0]}')
+    print(f'Самый умный герой {intel_list[0][1]}')
 
 most_intelligence(['Hulk', 'Captain America', 'Thanos'])
