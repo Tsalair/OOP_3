0da149093d5668261bb1845af535dc6056e22160
tsalair <toplider888@mail.ru>
2023-10-04 01:10:29 +0300

    Task_2 changed2

--- Task_2.py
+++ Task_2.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 class YaUploader:
     def __init__(self, token: str):
@@ -8,18 +9,18 @@ class YaUploader:
         
         url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = {'Authorization': 'OAuth '+token}
-        params = {'path': file_path}
+        params = {'path': os.path.basename(file_path)}
         response = requests.get(url, headers=headers, params=params)
         data = response.json()
         if 200 <= response.status_code < 300:
             file_url = data['href']
-            with open('Task_2.py', 'rb') as f:\
+            with open(file_path, 'rb') as f:
                 response = requests.post(file_url, files={'file': f})
             print(response.status_code)
 
 if __name__ == '__main__':
     
     path_to_file = input('Введите путь к файлу на вашем компьютере: ')
-    token = input('Введите token:' )
+    token = input('Введите token: ')
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
